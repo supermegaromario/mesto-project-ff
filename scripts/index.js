49d59b08d750f394dff573a6ff4d753397226dbd
Roman Borisov <supermegaromario@yandex.ru>
2023-12-12 14:49:17 +0300

    1212Review2

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,28 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
-const placesList = document.querySelector('.places__list');
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-function addCard(name, link, deleteCard) {
-const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-const deleteButton = cardElement.querySelector('.card__delete-button');
-cardElement.querySelector('.card__image').src = link;
-cardElement.querySelector('.card__title').textContent = name;
-cardElement.querySelector('.card__image').alt = name;
-deleteButton.addEventListener('click', deleteCard);
-return cardElement;
+function createCard(name, link, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  const imageElement = cardElement.querySelector(".card__image");
+  imageElement.src = link;
+  cardElement.querySelector(".card__title").textContent = name;
+  imageElement.alt = name;
+  deleteButton.addEventListener("click", deleteCard);
+  return cardElement;
 }
 
 // @todo: Функция удаления карточки
-function cardDelete(event) {
-  const listElement = event.target.closest('.card');
+function deleteCard(event) {
+  const listElement = event.target.closest(".card");
   listElement.remove();
-};
+}
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(item => placesList.append(addCard(item.name, item.link, cardDelete)));
+initialCards.forEach((item) =>
+  placesList.append(createCard(item.name, item.link, deleteCard))
+);
